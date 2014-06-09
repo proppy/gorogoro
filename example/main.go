a06cb8459aa80e0372edb6040f2c0b954c8ef18e
Johan Euphrosine <proppy@google.com>
2014-06-08 17:28:13 -0700

    add example

---
+++ example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, fmt.Sprintf("/%s", "World"), http.StatusFound)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<html><body>Hello, %s! 세상아 안녕!</body></html>", mux.Vars(r)["who"])
+}
+
+func main() {
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", handle).Methods("GET")
+	r.HandleFunc("/{who}", hello).Methods("GET")
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
